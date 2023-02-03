b75ba337e93535834b64f8e3e007131d82467cfb
Risab <risabvishwakarma@hotmail.com>
2023-02-04 02:26:57 +0530

    user service done with path variable

--- user_service/src/main/java/com/unitral/notification_service/controler/Main_Controler.java
+++ user_service/src/main/java/com/unitral/notification_service/controler/Main_Controler.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,7 +34,7 @@ public class Main_Controler {
 	
 	
 	@GetMapping("/{id}")
-	public Optional<User> getUser(@RequestParam int id){
+	public Optional<User> getUser(@PathVariable int id){
 		
 		return service.getUser(id);
 	}
@@ -54,7 +55,7 @@ public class Main_Controler {
 	
 	
 	@DeleteMapping("/{id}")
-	public void deleteUser(@RequestParam int id){
+	public void deleteUser(@PathVariable int id){
 		
 		service.deleteUser(id);
 	}
